75e934fd32fe65ff57a51b03e149c67edcf71ef8
Dave Jain <jain.udbhav@gmail.com>
2021-01-24 16:31:10 +1100

    Update longest-palindrome.py

--- medium/longest-palindrome.py
+++ medium/longest-palindrome.py
@@ -1,6 +1,19 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
+        posn = {}
+        
+        for c in range(10):
+            posn[str(c)] = []
+        for c in string.ascii_lowercase:
+            posn[c] = []
+        
+        for c in string.ascii_uppercase:
+            posn[c] = []
+        
+        for i,c in enumerate(s):
+            posn[c].append(i)
+            
         rs = s[::-1]
         ref = len(s)
         lmax = 0
@@ -10,21 +23,19 @@ class Solution:
         
         for i, c in enumerate(s):
             j = ref
+            if lmax > max(posn[c])-i+1:
+                continue
             
-            if lmax > ref-i-1:
-                break
-                
-            while j > i-1:
-                if s[j-1] == s[i]:
+            for j in posn[c]:
+                if j > i-1:
                     comp = set()
-                    subs = s[i:j]
+                    subs = s[i:j+1]
                     lsub = len(subs)
                     
-                    comp.add(rs[ref-j:ref-i])
-                    
+                    comp.add(rs[ref-j-1:ref-i])
                     if lsub > lmax and subs in comp:
                         start = i
-                        end = j
+                        end = j + 1
                         lmax = lsub
                 j = j -1
 
