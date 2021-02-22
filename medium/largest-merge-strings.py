678f107584c2c1833f7e9801b08e418ef1f30314
Dave Jain <jain.udbhav@gmail.com>
2021-02-22 14:53:36 +1100

    Update largest-merge-strings.py

--- medium/largest-merge-strings.py
+++ medium/largest-merge-strings.py
@@ -2,13 +2,14 @@ class Solution:
     def largestMerge(self, word1: str, word2: str) -> str:
         a = list(word1)
         b = list(word2)
-        c = []
-        while len(a) >0 or len(b) > 0:
+        c = ''
+        
+        while len(a) > 0 or len(b) > 0:
             if a > b:
-                c.append(a[0])
+                c += a[0]
                 del a[0]
             else:
-                c.append(b[0])
+                c += b[0]
                 del b[0]
         
-        return ''.join(c)
+        return c
